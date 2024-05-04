161b7d3b13a6650843bc6f192d7411fc9c2bf688
Mai Rodrigo <maialrodrigo@gmail.com>
2024-05-04 16:29:46 -0300

    Agrego productos + estilos css

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 //Aqui el cliente iniciaría la sesion con el usuario asignado por la empresa y la password. Si no es un cliente registrado, no podrá pedir presupuesto a traves del carrito.
-let clienteDeAlta = "Liga";
+/* let clienteDeAlta = "Liga";
 let passCliente = "123456";
 
 let usuario = prompt("Ingrese su usuario para acceder al sistema");
 let password = prompt("Ingrese la contraseña");
 
 if (usuario == clienteDeAlta && password === passCliente) {
-	alert("Bienvenido "+ clienteDeAlta +" !");
+	alert("Bienvenido " + clienteDeAlta + " !");
 } else {
 	alert(
 		"Veras el sitio completo unicamente si es un cliente registrado. Mas información contactanos al 1122334455"
 	);
-}
+} */
 
 //Carrito
 const productos = [
@@ -30,13 +30,11 @@ while (seleccion != "si" && seleccion != "no") {
 if (seleccion == "si") {
 	alert("A continuacion tendrá nuestra lista de productos");
 	console.log(productos);
-	
-}else{
-	alert("OK! Esperamos que le guste nuestro sitio")
+} else {
+	alert("OK! Esperamos que le guste nuestro sitio");
 }
 
-function agregarAlCarrito (producto, precio){
-    carrito.push ({producto, precio})
-    alert ('"${producto}" se agregó a su pedido')
+function agregarAlCarrito(producto, precio) {
+	carrito.push({ producto, precio });
+	alert('"${producto}" se agregó a su pedido');
 }
-

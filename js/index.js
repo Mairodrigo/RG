310dd15862eb3ce77612efd273a8ccf69368a5de
Mai Rodrigo <maialrodrigo@gmail.com>
2024-05-05 12:57:05 -0300

    JS agrego archivo de productos por separado + DOM a js principal

--- js/index.js
+++ js/index.js
@@ -1,41 +1,20 @@
-//Aqui el cliente iniciaría la sesion con el usuario asignado por la empresa y la password. Si no es un cliente registrado, no podrá pedir presupuesto a traves del carrito.
-/* let clienteDeAlta = "Liga";
-let passCliente = "123456";
-
-let usuario = prompt("Ingrese su usuario para acceder al sistema");
-let password = prompt("Ingrese la contraseña");
-
-if (usuario == clienteDeAlta && password === passCliente) {
-	alert("Bienvenido " + clienteDeAlta + " !");
-} else {
-	alert(
-		"Veras el sitio completo unicamente si es un cliente registrado. Mas información contactanos al 1122334455"
-	);
-} 
-let seleccion = prompt("Desea iniciar su pedido?");
-while (seleccion != "si" && seleccion != "no") {
-	alert("Por favor, ingrese si o no");
-	seleccion = prompt("Desea iniciar su pedido si o no?");
-}
-if (seleccion == "si") {
-	alert("A continuacion tendrá nuestra lista de productos");
-	console.log(productos);
-} else {
-	alert("OK! Esperamos que le guste nuestro sitio");
-}*/
-
-//Carrito
-const productos = [
-	{ nombre: "economizador", precio: 10000 },
-	{ nombre: "miniequipo", precio: 150000 },
-	{ nombre: "picos", precio: 8000 },
-];
 
 let carrito = [];
 
+productos.forEach((product) => {
+	let content = document.createElement("div");
+	content.className = "card";
+	content.innerHTML = `
+	<img class= productoImg src ="${product.img}">
+	<h3 class="nombreProducto">${product.nombre}</h3>
+	<p class="precioProducto">$ ${product.precio}</p>
+	`;
 
+	shopContent.append(content);
 
-function agregarAlCarrito(producto, precio) {
-	carrito.push({ producto, precio });
-	alert('"${producto}" se agregó a su pedido');
-}
+	let comprar = document.createElement ("button");
+	comprar.innerText = "COMPRAR";
+	comprar.className = "comprar";
+
+	content.append(comprar);
+});

7b80079c06a444b89ee7e9868891bf327314f575
Mai Rodrigo <maialrodrigo@gmail.com>
2024-05-05 18:45:45 -0300

    JS creo constantes para funcionalidad de carrito y modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const shopContent = document.getElementById ("shopContent");
+const verCarrito = document.getElementById ("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
 
 let carrito = [];
 
@@ -12,9 +15,33 @@ productos.forEach((product) => {
 
 	shopContent.append(content);
 
-	let comprar = document.createElement ("button");
+	let comprar = document.createElement("button");
 	comprar.innerText = "COMPRAR";
 	comprar.className = "comprar";
 
 	content.append(comprar);
+
+	comprar.addEventListener("click", () => {
+		carrito.push({
+			id: product.id,
+			img: product.img,
+			nombre: product.nombre,
+			precio: product.precio,
+		});
+		console.log(carrito);
+	});
+});
+
+verCarrito.addEventListener("click", () => {
+	const modalHeader = document.createElement("div")
+	modalHeader.className = "modal-header"
+	modalHeader.innerHTML = `
+	<h4 class="modal-header-title"> Carrito </h4>
+	`;
+	modalContainer.append(modalHeader);
+
+	const modalButton = document.createElement ("h4");
+	modalButton.innerText = "X";
+	modalButton.className = "modal-header-button";
+	modalHeader.append(modalButton);
 });
